105ab4d586fa51811c392b2d212ea88656ecba7c
onsdagens <pawdzi-7@student.ltu.se>
2023-12-15 14:20:06 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,63 @@
+use riscv_peripheral::clic::*;
+
+riscv_peripheral::clic_codegen!(base 0x5000);
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[repr(u16)]
+pub enum Interrupt {
+    GPIO = 1,
+    TIMER = 2,
+    SOME = 3,
+}
+
+unsafe impl InterruptNumber for Interrupt {
+    const MAX_INTERRUPT_NUMBER: u16 = 3;
+
+    #[inline]
+    fn number(self) -> u16 {
+        self as _
+    }
+
+    #[inline]
+    fn from_number(number: u16) -> Result<Self, u16> {
+        if number == 0 || number > Self::MAX_INTERRUPT_NUMBER {
+            Err(number)
+        }
+        else  {
+            // SAFETY: valid interrupt number
+            Ok(unsafe { core::mem::transmute(number)})
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[repr(u16)]
+pub enum Priority {
+    P0 = 0,
+    P1 = 1, 
+    P2 = 2, 
+    P3 = 3,
+    P4 = 4,
+    P5 = 5,
+    P6 = 6,
+    P7 = 7,
+    //etc...
+}
+
+unsafe impl PriorityNumber for Priority {
+    const MAX_PRIORITY_NUMBER: u8 = 7;
+
+    #[inline]
+    fn number(self) -> u8 {
+        self as _
+    }
+
+    #[inline]
+    fn from_number(number: u8) -> Result<Self, u8> {
+        if number > Self::MAX_PRIORITY_NUMBER {
+            Err(number)
+        } else {
+            // SAFETY: valid priority number
+            Ok(unsafe { core::mem::transmute(number) })
+        }
+    }
+}
